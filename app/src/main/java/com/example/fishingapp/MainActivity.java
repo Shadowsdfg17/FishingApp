e7a56db9d88fd3e609bd1e0370cec8edba49cf68
Andrea <Shadowsdfg17>
2020-10-30 14:20:28 +0100

    Initial commit

---
+++ app/src/main/java/com/example/fishingapp/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.fishingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+
+        new Timer().schedule(new TimerTask() {
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(), listActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
+    }
+}
